f3fe4dfb3830754e7768fa47d93367f604d92083
Matthias BRAUN <Matthias.BRAUN@bachmann.info>
2023-11-24 13:10:43 +0100

    Added Project

---
+++ ChatAppServer1.0/Models/Chat.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatAppServer1._0.Models
+{
+    public class Chat
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int ChatId { get; set; }
+
+        public int SenderId { get; set; }
+
+        public int ReceiverId { get; set; }
+        public string Message { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
